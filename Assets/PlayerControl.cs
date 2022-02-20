c2026cd818dd49a25b956c89f9898693cde17156
634_3 <k020g1146@g.neec.ac.jp>
2022-02-20 22:32:52 +0900

    キャラ移動

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("各種ステータス")]
+    [SerializeField] private float _NormalSpeed = 6f;
+
+
+    private float Speed = 0f;
+
+
+    private float XSpeed = 0f;
+    private float YSpeed = 0f;
+
+
+    [Header("コンポーネント")]
+    [SerializeField] private Rigidbody2D _rb = null;
+
+    private void Start()
+    {
+        Speed = _NormalSpeed;
+    }
+
+    private void LateUpdate()
+    {
+        XSpeed = Input.GetAxis("Horizontal") * Speed;
+        YSpeed = Input.GetAxis("Vertical") * Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector2(XSpeed, YSpeed);
+    }
+}
