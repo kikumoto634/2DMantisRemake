55560a88e0581a576af6b75df698f727b18c1b4a
634_3 <k020g1146@g.neec.ac.jp>
2022-02-22 22:32:51 +0900

    敵の仮挙動　発射型

--- Assets/Character/TestEnemy/Test_EnemyControl.cs
+++ Assets/Character/TestEnemy/Test_EnemyControl.cs
@@ -4,14 +4,16 @@ public class Test_EnemyControl : MonoBehaviour
 {
     [SerializeField] private enum EnemyType
     { 
-        Wandering,  //放浪
-        Tracking    //追跡
+        Wandering,  //放浪(攻撃無)
+        Tracking,   //追跡(近距離)
+        Firing      //発射(遠距離)
     };
     [SerializeField] private EnemyType CurrentEnemyType = EnemyType.Wandering;
 
 
     [Header("基礎ステータス")]
-    [SerializeField] private float _NormalSpeed = 5f;
+    [SerializeField] private float _NormalSpeed = 5f;   //放浪、追跡
+    [SerializeField]private float _SlowSpeed = 2.5f;    //発射
 
     //速度
     private float Speed = 0f;
@@ -40,10 +42,21 @@ public class Test_EnemyControl : MonoBehaviour
 
     private void Start()
     {
+        switch (CurrentEnemyType)
+        {
+            case EnemyType.Firing:
+                Speed = _SlowSpeed;
+
+                break;
+
+            default:
+                Speed = _NormalSpeed;
+
+                break;
+        }
+
         thisTransform = this.transform;
         playerTransform = _player.transform;
-
-        Speed = _NormalSpeed;
 
         //初期移動設定
         XDirection = Random.Range(-1f, 1f);
@@ -56,12 +69,13 @@ public class Test_EnemyControl : MonoBehaviour
 
     private void LateUpdate()
     {
+        //距離測定
+        distance = playerTransform.position - thisTransform.position;
+
         switch (CurrentEnemyType)
         { 
             case EnemyType.Tracking:
 
-                //距離測定
-                distance = playerTransform.position - thisTransform.position;
                 if (distance.magnitude <= Mathf.Abs(8f))
                 {
                     //追跡モード移行
@@ -73,6 +87,25 @@ public class Test_EnemyControl : MonoBehaviour
                     //速度の再設定
                     Speed = _NormalSpeed;
                     _rb.velocity = _rb.velocity.normalized * Speed;
+                }
+
+                break;
+
+            case EnemyType.Firing:
+
+                if(distance.magnitude <= Mathf.Abs(10f))
+                {
+                    //発射モード
+                    Vector3 diff = (playerTransform.position - thisTransform.position).normalized;
+                    thisTransform.rotation = Quaternion.FromToRotation(Vector3.up, diff);
+                    Speed = 0f;
+                    _rb.velocity = -(_rb.velocity.normalized) * Speed;
+                }
+                else if (distance.magnitude > Mathf.Abs(10f))
+                { 
+                    //速度の再設定
+                    Speed = _SlowSpeed;
+                    _rb.velocity = thisTransform.up * Speed;
                 }
 
                 break;
--- Assets/PostEffect/PerlinNoise.shader
+++ Assets/PostEffect/PerlinNoise.shader
@@ -2,21 +2,17 @@ Shader "Unlit/PerlinNoise"
 {
     Properties
     {
-        _MainTex ("Texture", 2D) = "white" {}
+        _MainTex ("Texture", 2D) = "" {}
     }
+
     SubShader
     {
-        Tags { "RenderType"="Opaque" }
-        LOD 100
-
         Pass
         {
+
             CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            // make fog work
-            #pragma multi_compile_fog
-
             #include "UnityCG.cginc"
 
             struct appdata
@@ -27,30 +23,52 @@ Shader "Unlit/PerlinNoise"
 
             struct v2f
             {
-                float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
             };
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
 
+
             v2f vert (appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                UNITY_TRANSFER_FOG(o,o.vertex);
+                o.uv = v.uv;
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
-                fixed4 col = tex2D(_MainTex, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
+                //モザイク処理
+                /*float density = 50;
+                fixed4 col = tex2D(_MainTex, floor(i.uv * density) / density);
+                return col;*/
+
+
+                //モザイク処理（正方形）
+                float density = 10;
+                fixed4 col = tex2D(_MainTex,
+                    floor(i.uv * _ScreenParams.xy / density) * density / _ScreenParams.xy);
                 return col;
+
+
+                //スキャンノイズ
+                /*float _Speed = 0.5;
+                float _Width = 0.025;
+                float _Power = 0.25;
+
+                float sbTime = frac(_Time.y * _Speed);
+                float seTime = sbTime + _Width;
+                float2 uv = float2(
+                    i.uv.x + sin(smoothstep(sbTime, seTime, i.uv.y) * 2 * 3.14159) * _Power,
+                    i.uv.y
+                );
+
+                fixed4 col = tex2D(_MainTex, uv);
+                return col;*/
+
             }
             ENDCG
         }
