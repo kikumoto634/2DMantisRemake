99d7af02582eae6a9f1040bd4dfc88ded47454a7
634_3 <k020g1146@g.neec.ac.jp>
2022-03-02 01:03:41 +0900

    playerの居合攻撃実装

--- Assets/Character/Player/PlayerControl.cs
+++ Assets/Character/Player/PlayerControl.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [Header("各種ステータス")]
-    public float _NormalSpeed = 6f;
+    [SerializeField]private float _NormalSpeed = 6f;
 
-    public float _SlashSpeed = 6f;
+    [SerializeField]private float _SlashSpeed = 6f;
 
     private float Speed = 0f;
     private float XSpeed = 0f;
@@ -13,63 +13,90 @@ public class PlayerControl : MonoBehaviour
 
 
     bool IsAttack = false;
+
     bool IsSlash = false;
+    bool IsGo = false;
 
     [Header("コンポーネント")]
-    public Rigidbody2D _rb = null;
+    private Rigidbody2D _rb = null;
+    private BoxCollider2D _bc = null;
 
-    public Animator _slashAnim = null;
+    [SerializeField]private Animator _slashAnim = null;
 
-    public Animator _slashAreaAnim = null;
-    public BoxCollider2D _slashCollider = null;
+    [SerializeField]private Animator _slashAreaAnim = null;
+    [SerializeField]private BoxCollider2D _slashCollider = null;
 
 
     private Vector3 cameraForward = default;
     private Vector3 moveForward = default;
 
+    private Vector3 SavePos = default;
+
     private void Start()
     {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _bc = gameObject.GetComponent<BoxCollider2D>();
+
         Speed = _NormalSpeed;
     }
 
     private void Update()
     {
-        if (!IsAttack)
+        /*off:通常、on:居合移動時の操作不能*/
+        if(!IsGo)
         {
-            XSpeed = Input.GetAxis("Horizontal");
-            YSpeed = Input.GetAxis("Vertical");
+            //移動
+            if (!IsAttack)
+            {
+                XSpeed = Input.GetAxis("Horizontal");
+                YSpeed = Input.GetAxis("Vertical");
+            }
+            else if (IsAttack)
+            { 
+                XSpeed = 0f;
+                YSpeed = 0f;
+            }
+
+            //攻撃居合
+            if (!IsSlash && Input.GetKeyDown(KeyCode.Backspace))
+            {
+                IsSlash = true;
+                Speed /= 2f;
+            }
+            else if (IsSlash && Input.GetKeyUp(KeyCode.Backspace))
+            {
+                IsGo = true;
+                _bc.isTrigger = true;
+                _slashCollider.enabled = true;
+
+                SavePos = transform.position + Vector3.Scale(transform.up, new Vector3(_SlashSpeed, _SlashSpeed, 0));
+                Speed *= 2f;
+
+                IsSlash = false;
+            }
+            //攻撃
+            if (!IsAttack && Input.GetKeyDown(KeyCode.Return))   IsAttack = true;
+
         }
-        else if (IsAttack)
+        else if (IsGo)
         { 
-            XSpeed = 0f;
-            YSpeed = 0f;
-        }
+            transform.position = Vector2.MoveTowards(transform.position, SavePos, 64f * Time.deltaTime);
 
+            if (transform.position == SavePos)
+            { 
+                SavePos = default;
 
-        if(!IsSlash && Input.GetKeyDown(KeyCode.Backspace))
-        {
-            IsSlash = true;
-            Speed /= 2f;
-        }
-        if(IsSlash && Input.GetKeyUp(KeyCode.Backspace))
-        {
-            _slashCollider.enabled = true;
-            transform.position = Vector2.MoveTowards(transform.position, transform.position + Vector3.Scale(transform.up, new Vector3(_SlashSpeed, _SlashSpeed, 0)), _SlashSpeed);
-            Speed *= 2f;
-            IsSlash = false;
-        }
-        if(!IsAttack && Input.GetKeyDown(KeyCode.Return))   IsAttack = true;
+                _bc.isTrigger = false;
+                _slashCollider.enabled = false;
 
+                IsGo = false;
+            }
+
+        }
     }
 
     private void LateUpdate()
     {
-
-        if(_slashCollider.enabled)  
-        {
-            Debug.Log("Off");
-            _slashCollider.enabled = false;
-        }
 
         //アニメーション処理
         _slashAnim.SetBool("IsAttack", IsAttack);
@@ -83,12 +110,14 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //カメラから正面方向を取得
         cameraForward = Vector3.Scale(Camera.main.transform.up, new Vector3(1, 1, 0)).normalized;
- 
+        //キャラの前方方向を取得
         moveForward = cameraForward * YSpeed + Camera.main.transform.right * XSpeed;
  
         _rb.velocity = moveForward * Speed + new Vector3(0, 0, 0);
  
+        //移動時のみ回転を可能にする
         if (moveForward != Vector3.zero) {
             transform.rotation = Quaternion.FromToRotation(Vector3.up, moveForward);
         }
